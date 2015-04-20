0d1d286a0b8e40fbe63ccd291fe67d391a3c52fc
Yajaira More Prada <amore_yp_03@hotmail.com>
2015-04-19 20:37:32 -0500

    Tab2
    arreglando tab 1 , efecto y tab 2

--- js/script.js
+++ js/script.js
@@ -10,5 +10,13 @@ $(document).ready(
 				width:920,
 			}
 		});
+		
+		$("ul.tabs li").click(function() {
+		$("ul.tabs li").removeClass("active");
+		$(this).addClass("active");
+		$(".contenido-tab").hide();
+		var tabActual = $(this).find("a").attr("href");
+		$(tabActual).fadeIn();
+	});
 	}
 );
