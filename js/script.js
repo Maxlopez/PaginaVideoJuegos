22a41eb90a8688452704cd3012a858345b627798
Yajaira More Prada <amore_yp_03@hotmail.com>
2015-04-19 22:35:18 -0500

    script
    script identado

--- js/script.js
+++ js/script.js
@@ -12,11 +12,11 @@ $(document).ready(
 		});
 		
 		$("ul.tabs li").click(function() {
-		$("ul.tabs li").removeClass("active");
-		$(this).addClass("active");
-		$(".contenido-tab").hide();
-		var tabActual = $(this).find("a").attr("href");
-		$(tabActual).fadeIn();
-	});
+			$("ul.tabs li").removeClass("active");
+			$(this).addClass("active");
+			$(".contenido-tab").hide();
+			var tabActual = $(this).find("a").attr("href");
+			$(tabActual).fadeIn();
+		});
 	}
 );
