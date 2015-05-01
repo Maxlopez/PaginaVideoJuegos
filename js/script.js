3791fa8abc7605cf258f1bc2a3df616ff099f37b
HubertMestanza <hubert.mestanza@gmail.com>
2015-05-01 12:44:14 -0500

    Mostrar Contenido
    muestra y oculta contenido

--- js/script.js
+++ js/script.js
@@ -20,5 +20,35 @@ $(document).ready(
 			$(tabActual).fadeIn(1500);
 			return false;
 		});
+		
+		$("a.mostrar2").click(function() {
+			$("#newPlataforma2 #ver2").show();
+				
+			$("#mostrar2").text("Ocultar Contenido");
+			
+			Ocultar();
+			return false;
+		});
+		
+		function Mostrar(){
+			$("a.mostrar2").click(function() {
+			$("#newPlataforma2 #ver2").show();
+				
+			$("#mostrar2").text("Ocultar Contenido");
+			
+			Ocultar();
+			return false;
+			});
+		}
+		
+		function Ocultar(){
+			$("a.mostrar2").click(function(){
+				$("#newPlataforma2 #ver2").hide();	
+					
+				$("#mostrar2").text("Mostrar Contenido");
+				Mostrar();
+				return false;
+			});
+		}
 	}
 );
